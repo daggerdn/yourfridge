dd92b5650d1c35de0eb4f0e9a2cb5a624ac50c1c
Damian Nowak <damian@devnowak.pl>
2020-07-02 15:53:12 +0200

    Add addNewProduct method to insert products
    This method adds only one product currently.

--- src/main/java/pl/devnowak/yourfridge/controller/ProductController.java
+++ src/main/java/pl/devnowak/yourfridge/controller/ProductController.java
@@ -3,8 +3,13 @@ package pl.devnowak.yourfridge.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import pl.devnowak.yourfridge.entity.Product;
+import pl.devnowak.yourfridge.entity.ProductCategory;
 import pl.devnowak.yourfridge.model.ProductRepository;
+
+import java.time.LocalDate;
 
 @Controller
 @RequestMapping("product")
@@ -20,4 +25,18 @@ public class ProductController {
 
         return "product/index";
     }
+
+    @GetMapping("add")
+    public String addNewProduct() {
+        Product newProduct = new Product();
+        newProduct.setName("Milk");
+        newProduct.setExpirationDate(LocalDate.of(2020, 7, 10));
+        newProduct.setDayOfPurchase(LocalDate.of(2020, 7, 4));
+        newProduct.setCategory(ProductCategory.DAIRY);
+        newProduct.setShop("Tesco");
+
+        productRepository.save(newProduct);
+        return "redirect:";
+    }
+
 }
