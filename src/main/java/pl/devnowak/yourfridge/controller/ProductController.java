9ac622cf345c748d64cece00f703244992c6742e
Damian Nowak <damian@devnowak.pl>
2020-07-02 15:29:09 +0200

    Add index method to get all products on main page

--- src/main/java/pl/devnowak/yourfridge/controller/ProductController.java
+++ src/main/java/pl/devnowak/yourfridge/controller/ProductController.java
@@ -1,5 +1,23 @@
 package pl.devnowak.yourfridge.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import pl.devnowak.yourfridge.model.ProductRepository;
+
+@Controller
+@RequestMapping("product")
 public class ProductController {
-    
+
+    @Autowired
+    private ProductRepository productRepository;
+
+    @RequestMapping(value = "")
+    public String index(Model model) {
+
+        model.addAttribute("products", productRepository.findAll());
+
+        return "product/index";
+    }
 }
