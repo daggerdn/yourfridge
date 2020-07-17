1c5009c9cbb10f64489e5e21306e7863d84244b2
Damian Nowak <damian@devnowak.pl>
2020-07-17 03:22:21 +0200

    Add implementation of ProductService class

--- src/main/java/pl/devnowak/yourfridge/model/ProductService.java
+++ src/main/java/pl/devnowak/yourfridge/model/ProductService.java
@@ -1,6 +1,31 @@
 package pl.devnowak.yourfridge.model;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import pl.devnowak.yourfridge.entity.Product;
+
+import java.util.List;
+
+@Service
 public class ProductService {
 
-    
+    private final ProductRepository productRepository;
+
+    @Autowired
+    public ProductService(ProductRepository productRepository) {
+        this.productRepository = productRepository;
+
+    }
+
+    public List<Product> getAllProducts() {
+        return productRepository.findAll();
+    }
+
+    public void saveProduct(Product product) {
+        productRepository.save(product);
+    }
+
+    public void deleteProductById(Long productId) {
+        productRepository.deleteById(productId);
+    }
 }
